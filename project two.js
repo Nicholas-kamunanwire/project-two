1f3ed79d212508cf29e8223ac5ded0916497fd54
Kamunanwire Nicholas <nicholaskamunanwire@gmail.com>
2021-11-19 15:22:53 +0000

    Added Java script

---
+++ project two.js
@@ -0,0 +1,7 @@
+alert('Hello. Welcome to my web page.😎 ✌️');
+let surName= prompt("What is your surname name? 👇 ⬇️");
+let lastName = prompt("and your last name? 👇 ⬇️")
+alert(`${surName} ${lastName}. Welcome to my webpage and hope you enjoy it 👊 🤙.`);
+
+
+
