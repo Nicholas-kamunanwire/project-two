d17e4b92dce5e6dc0cbc1f080b4496b168482836
Kamunanwire Nicholas <nicholaskamunanwire@gmail.com>
2021-11-30 19:50:48 +0000

    Added some java script on age

--- project two.js
+++ project two.js
@@ -1,13 +1,44 @@
 alert('Hello. Welcome to my web page.😎 ✌️');
 let usersAge = prompt('How old are you ?');
-if(usersAge % 2 === 0){
-    alert('Your age is Even');
-} 
-else if(usersAge % 2 === 1){
-    alert('Your age is Odd');
+// if(usersAge % 2 === 0){
+//     alert('Your age is Even');
+// } 
+// else if(usersAge % 2 === 1){
+//     alert('Your age is Odd');
+// }
+// else{
+//     alert('Your age is niether Even nor Odd');  
+// }
+// if (usersAge % 3 ===0 && usersAge % 5 ===0 ){
+//     alert('FizzBuzz');
+// }
+// else if (usersAge % 3 === 0){
+//     alert('Fizz');
+// }
+// else if(usersAge % 5===0){
+//     alert('Buzz');
+// }
+ 
+// else{
+//     alert('Your age is not divisable by 3 and 5');
+// }
+if (usersAge< 0){
+    alert('🙅Please put the correct age🙅');
+}
+else if (usersAge < 5){
+    alert('You are a baby👶');
+}
+else if (usersAge< 13){
+    alert('You are a child🧒');
+}
+else if (usersAge< 20){
+    alert('You are a teenager😎');
+}
+else if (usersAge > 20){
+    alert('You are an Adult👨🧓 👩👵');
 }
 else{
-    alert('Your age is niether Even nor Odd');
+    alert('🙅please put the correct age🙅');
 }
 
 
